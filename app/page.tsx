19a889bcb2741b38a639de63e61393fecd42ff0a
Ramadhika123 <ramadhikadarma0607@gmail.com>
2025-05-18 19:15:47 +0700

    Membenarkan Untuk Redirect ke Page Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import ConnectSupabaseSteps from '@/components/tutorial/connect-supabase-steps';
 import SignUpUserSteps from '@/components/tutorial/sign-up-user-steps';
 import { hasEnvVars } from '@/utils/supabase/check-env-vars';
+import { redirect } from 'next/navigation'; // Gunakan dari next/navigation, bukan next/dist/server/api-utils
 
 export default async function Home() {
-  return (
-    <>
-      <main className="flex-1 flex flex-col gap-6 px-4">{hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}</main>
-    </>
-  );
+  // Redirect ke halaman /home
+  redirect('/home');
+  
+  // Kode di bawah ini tidak akan dijalankan karena redirect menghentikan eksekusi fungsi
+  // return (
+  //   <>
+  //     <main className="flex-1 flex flex-col gap-6 px-4">
+  //       {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+  //     </main>
+  //   </>
+  // );
 }
