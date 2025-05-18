ca41f7c60c2a7631b990eceb040a6ccb4cb03115
Ramadhika123 <ramadhikadarma0607@gmail.com>
2025-05-18 19:54:59 +0700

    fix: Membenarkan Root Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,5 @@ import { hasEnvVars } from '@/utils/supabase/check-env-vars';
 import { redirect } from 'next/navigation'; // Gunakan dari next/navigation, bukan next/dist/server/api-utils
 
 export default async function Home() {
-  // Redirect ke halaman /home
   redirect('/home');
-  
-  // Kode di bawah ini tidak akan dijalankan karena redirect menghentikan eksekusi fungsi
-  // return (
-  //   <>
-  //     <main className="flex-1 flex flex-col gap-6 px-4">
-  //       {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-  //     </main>
-  //   </>
-  // );
 }
